24d9df7bd9f4108a1bec51e8e389f4b859213f64
craigwann <craigwann@me.com>
2018-10-01 11:55:08 -0700

    basic whack a ruby game function

---
+++ whack/whack.rb
@@ -0,0 +1,61 @@
+require 'gosu'
+
+class WhackARuby < Gosu::Window
+    def initialize
+        super(800,600)
+        self.caption = 'Whack the Ruby' 
+        @image = Gosu::Image.new(self,'ruby.png',false)
+        @x = 200
+        @y = 200
+        @width = 50
+        @height = 43
+        @velocity_x = 5
+        @velocity_y = 5
+        @visible = 0
+        @hammer_image = Gosu::Image.new('hammer.png')
+        @hit = 0
+
+    end
+
+    def update
+        @x += @velocity_x
+        @y += @velocity_y
+        @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
+        @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+        @visible -= 1
+        @visible = 60 if @visible < -10 && rand < 0.01
+        @time_left = (100 - (Gosu.milliseconds / 1000))
+      end
+
+      def button_down(id)
+        if (id == Gosu::MsLeft)
+          if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
+            @hit = 1
+          else
+            @hit = -1
+          end
+        end
+      end
+    
+
+    def draw
+        if @visible > 0
+            @image.draw(@x - @width / 2, @y - @height / 2, 1)
+        end
+        @hammer_image.draw(mouse_x - 40, mouse_y - 10, 1)
+        if @hit == 0
+            c = Gosu::Color::NONE
+          elsif @hit == 1
+            c = Gosu::Color::GREEN
+          elsif @hit == -1
+            c = Gosu::Color::RED
+          end
+          draw_quad(0, 0, c, 800, 0, c, 800, 600, c, 0, 600, c)
+          @hit = 0
+
+    end
+
+end
+
+window = WhackARuby.new
+window.show
