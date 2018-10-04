bfc6b6551ab0c6e0a23fa81f40c95c0d7eaf291f
craigwann <craigwann@me.com>
2018-10-04 09:04:55 -0700

    update ships and slash screen

--- sector5/enemy.rb
+++ sector5/enemy.rb
@@ -3,7 +3,7 @@ class Enemy
     attr_reader :x, :y, :radius
 
     def initialize(window)
-      @radius = 20
+      @radius = 22
       @x = rand(window.width - 2 * @radius) + @radius
       @y = 0
       @image = Gosu::Image.new('images/enemy.png') 
